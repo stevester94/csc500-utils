a7b8a8fca1b777b5852f00a5d5f22bb9f56cdf2b
Steven Mackey <stevester94@gmail.com>
2021-04-18 20:19:05 -0700

    Add symbol accessor

--- datasetaccessor.py
+++ datasetaccessor.py
@@ -23,16 +23,33 @@ class DatasetAccessor(ABC):
         tfrecords_path="../vanilla_tfrecords/"
     ):
         self.tfrecords_paths = self.filter_datasets(get_tfrecords_in_dir(tfrecords_path))
+        self.tfrecords_paths.sort()
 
         if len(self.tfrecords_paths) == 0:
             print("No paths remained after filtering. Time to freak out!")
             sys.exit(1)
+
+        pprint(self.tfrecords_paths)
         
         dataset = tf.data.Dataset.from_tensor_slices(self.tfrecords_paths)
+        dataset = dataset.shuffle(dataset.cardinality(), seed=1337, reshuffle_each_iteration=True)
 
-        self.dataset = dataset.interleave(lambda path:
-           tf.data.TFRecordDataset(path).map(self.parse_serialized_example, num_parallel_calls=1),
-            cycle_length=4, block_length=16)
+        # SM: OK let's unpack what's going on here. 
+        # We start with 'dataset' which is a dataset of file paths.
+        # We then use 'interleave to apply a map function across this dataset of file paths and then interleave the results
+        # The map function itself is creating a TFRectordDataset from the path, which itself is then calling a map function to deserialize the TFRecords
+        # block_length: How many elements we want to pull from each dataset before going to the next
+        #               Note that this is a little wonky since each TFRecord path is a file that only contains a single TFRecord. However, this may
+        #               not always be the case. So to achieve good shuffling block length should be 1 
+        # cycle_length: Should be set to the number of datasets. This is how many original datasets we operate on at the same time.
+        #               So if we had 10 datasets we are interleaving, and block length one, but cycle length = 5, we'd pull one element
+        #               from each of those 5 datasets before moving to the next set of 5.
+        self.dataset = dataset.interleave(
+            tf.data.TFRecordDataset,
+            cycle_length=dataset.cardinality(), 
+            block_length=1
+        )
+        self.dataset = self.dataset.map(self.parse_serialized_example, num_parallel_calls=tf.data.AUTOTUNE, deterministic=True)
 
     def get_dataset(self):
         return self.dataset
@@ -116,6 +133,71 @@ class VanillaDatasetAccessor(DatasetAccessor):
     def parse_serialized_example(self, example):
         return self.parse_serialized_transmission_example(example)
 
+
+class SymbolDatasetAccessor(DatasetAccessor):
+    def __init__(
+        self,
+        tfrecords_path="../symbol_tfrecords/",
+        day_to_get="All",
+        transmitter_id_to_get="All",
+        transmission_id_to_get="All"
+    ):
+        self.day_to_get = day_to_get
+        self.transmitter_id_to_get = transmitter_id_to_get
+        self.transmission_id_to_get = transmission_id_to_get
+
+        # Contantly re-instantiating this dict adds non-trivial overhead, so we save it
+        self._ofdm_symbol_example_description = {
+            'transmitter_id':     tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+            'day':                tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+            'transmission_id':    tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+            'frequency_domain_IQ':    tf.io.FixedLenFeature([], tf.string, default_value=''),
+            'frame_index':    tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+            'symbol_index':    tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+        }
+
+        super().__init__(tfrecords_path=tfrecords_path)
+
+    def parse_serialized_symbol_example(self, serialized_example):
+        parsed_example = tf.io.parse_single_example(serialized_example, self._ofdm_symbol_example_description)
+
+        parsed_example["frequency_domain_IQ"] = tf.io.parse_tensor(parsed_example["frequency_domain_IQ"], tf.float32)
+
+        return parsed_example
+
+    def is_any_word_in_string(self, list_of_words, string):
+        for w in list_of_words:
+            if w in string:
+                return True
+        return False
+
+    def filter_datasets(self, paths):
+        filtered_paths = paths
+        if self.day_to_get != "All":
+            assert(isinstance(self.day_to_get, list))
+            assert(len(self.day_to_get) > 0)
+            
+            filt = ["day-{}_".format(f) for f in self.day_to_get]
+            filtered_paths = [p for p in filtered_paths if self.is_any_word_in_string(filt, p)]
+
+        if self.transmitter_id_to_get != "All":
+            assert(isinstance(self.transmitter_id_to_get, list))
+            assert(len(self.transmitter_id_to_get) > 0)
+
+            filt = ["transmitter-{}_".format(f) for f in self.transmitter_id_to_get]
+            filtered_paths = [p for p in filtered_paths if self.is_any_word_in_string(filt, p)]
+
+        if self.transmission_id_to_get != "All":
+            assert(isinstance(self.transmission_id_to_get, list))
+            assert(len(self.transmission_id_to_get) > 0)
+
+            filt = ["transmission-{}.".format(f) for f in self.transmission_id_to_get]
+            filtered_paths = [p for p in filtered_paths if self.is_any_word_in_string(filt, p)]
+
+        return filtered_paths
+
+    def parse_serialized_example(self, example):
+        return self.parse_serialized_symbol_example(example)
 
 def unit_test():
     import random
