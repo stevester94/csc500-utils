b51f5b2affa1df3fe73ce2fc377315b6c4bc49bf
Steven Mackey <stevester94@gmail.com>
2021-05-16 13:21:56 -0700

    Add generic shuffled dataset accessor

--- steves_utils/dataset_shuffler.py
+++ steves_utils/dataset_shuffler.py
@@ -156,6 +156,82 @@ class Dataset_Shuffler:
             pw.close()
 
 
+def Shuffled_Dataset_Factory(
+    path,
+    train_val_test_splits,
+):
+    """Generates TF datasets for accessing large shuffled datasets.
+    train,val,test splits are done based on files rather than the commong take/skip method.
+
+    Reshuffling of train_ds is done via reshuffling the file names on each iteration (I have confirmed this works)
+
+    Yes I know it's not a factory.
+    """
+    files = utils.get_files_in_dir(path)
+
+    train_start_stop = (0, int(len(files)*train_val_test_splits[0]))
+    val_start_stop   = (train_start_stop[1], train_start_stop[1] +  int(len(files)*train_val_test_splits[1]))
+    test_start_stop  = (val_start_stop[1], val_start_stop[1] + int(len(files)*train_val_test_splits[2]))
+
+    train_files     = files[train_start_stop[0]:train_start_stop[1]]
+    val_files       = files[val_start_stop[0]:val_start_stop[1]]
+    test_files      = files[test_start_stop[0]:test_start_stop[1]]
+
+    if len(train_files) == 0:
+        raise Exception("Train files had no files, indicative of bad tran/val/test split or too few files to draw from")
+    if len(val_files) == 0:
+        raise Exception("Val files had no files, indicative of bad tran/val/test split or too few files to draw from")
+    if len(test_files) == 0:
+        raise Exception("Test files had no files, indicative of bad tran/val/test split or too few files to draw from")
+
+    train_ds = tf.data.Dataset.from_tensor_slices(train_files)
+    val_ds   = tf.data.Dataset.from_tensor_slices(val_files)
+    test_ds  = tf.data.Dataset.from_tensor_slices(test_files)
+
+    train_ds = train_ds.shuffle(train_ds.cardinality(), reshuffle_each_iteration=True)
+
+    # SM: OK let's unpack what's going on here. 
+    # We start with 'dataset' which is a dataset of file paths.
+    # We then use 'interleave to apply a map function across this dataset of file paths and then interleave the results
+    # The map function itself is creating a TFRectordDataset from the path. Normally you would then map this using your custom TFRecord deserialize
+    # function. This factory doesn't care about that, we leave it to the downstream user.
+    #
+    # Because the files are already shuffled, the "interleaving" only pulls from one file at a time (As determined by cycle length).
+    #     There may be performance gains if this is increased.
+    #
+    # Explanation (Note I wrote these when I was using interleaving as a means of shuffling, so they are in that context, not the current usage):
+    # block_length: How many elements we want to pull from each dataset before going to the next
+    #               Note that this is a little wonky since each TFRecord path is a file that only contains a single TFRecord. However, this may
+    #               not always be the case. So to achieve good shuffling block length should be 1 
+    # cycle_length: Should be set to the number of datasets. This is how many original datasets we operate on at the same time.
+    #               So if we had 10 datasets we are interleaving, and block length one, but cycle length = 5, we'd pull one element
+    #               from each of those 5 datasets before moving to the next set of 5.
+
+    train_ds = train_ds.interleave(
+        tf.data.TFRecordDataset,
+        cycle_length=1, 
+        block_length=1,
+        deterministic=True
+    )
+    val_ds = val_ds.interleave(
+        tf.data.TFRecordDataset,
+        cycle_length=1, 
+        block_length=1,
+        deterministic=True
+    )
+    test_ds = test_ds.interleave(
+        tf.data.TFRecordDataset,
+        cycle_length=1, 
+        block_length=1,
+        deterministic=True
+    )
+
+    return {
+        "train_ds": train_ds,
+        "val_ds": val_ds,
+        "test_ds": test_ds,
+    }
+
 
 if __name__ == "__main__":
     from steves_utils.ORACLE.simple_oracle_dataset_factory import Simple_ORACLE_Dataset_Factory
@@ -169,19 +245,19 @@ if __name__ == "__main__":
         serial_numbers_to_get=[ALL_SERIAL_NUMBERS[0]]
     )
 
-    shuffler = Dataset_Shuffler(
-        input_ds=ds,
-        one_example_to_tf_record_func=oracle_serialization.example_to_tf_record,
-        one_example_from_serialized_tf_record_func=oracle_serialization.serialized_tf_record_to_example,
-        batch_example_to_tf_record_func=oracle_serialization.example_to_tf_record,
-        output_batch_size=1000,
-        num_piles=5,
-        output_format_str="shuffled_batchSize-{batch_size}_part-{part}.tfrecord_ds",
-        output_max_file_size_MB=200,
-        pile_dir="/mnt/wd500GB/derp_2/pile",
-        output_dir="/mnt/wd500GB/derp_2/output",
-        seed=1337
-    )
+    # shuffler = Dataset_Shuffler(
+    #     input_ds=ds,
+    #     one_example_to_tf_record_func=oracle_serialization.example_to_tf_record,
+    #     one_example_from_serialized_tf_record_func=oracle_serialization.serialized_tf_record_to_example,
+    #     batch_example_to_tf_record_func=oracle_serialization.example_to_tf_record,
+    #     output_batch_size=1000,
+    #     num_piles=5,
+    #     output_format_str="shuffled_batchSize-{batch_size}_part-{part}.tfrecord_ds",
+    #     output_max_file_size_MB=200,
+    #     pile_dir="/mnt/wd500GB/derp_2/pile",
+    #     output_dir="/mnt/wd500GB/derp_2/output",
+    #     seed=1337
+    # )
     
     
     # p = "/mnt/wd500GB/derp/pile/pile_0"
@@ -193,7 +269,20 @@ if __name__ == "__main__":
     #         print(e["index_in_file"])
 
 
-    shuffler.create_and_check_dirs()
-    shuffler.write_piles()
-    print("Now shuffle")
-    shuffler.shuffle_piles()
+    # shuffler.create_and_check_dirs()
+    # shuffler.write_piles()
+    # print("Now shuffle")
+    # shuffler.shuffle_piles()
+
+    datasets = Shuffled_Dataset_Factory("/mnt/wd500GB/derp/output", train_val_test_splits=(0.6, 0.2, 0.2))
+
+    train_ds = datasets["train_ds"]
+    val_ds = datasets["val_ds"]
+    test_ds = datasets["test_ds"]
+
+    train_ds = train_ds.map(oracle_serialization.serialized_tf_record_to_example)
+
+    count = 0
+    for e in train_ds:
+        count += e["IQ"].shape[0]
+        print(count)
