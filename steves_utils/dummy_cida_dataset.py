05b466fe468e7e622edaa92472480b77f060610d
Steven Mackey <stevester94@gmail.com>
2021-10-17 17:08:12 -0700

    Pull in golden changes for dummy cida. Normalize correctly, and disambiguate x

--- steves_utils/dummy_cida_dataset.py
+++ steves_utils/dummy_cida_dataset.py
@@ -3,9 +3,10 @@
 import numpy as np
 import random
 import torch
+from torch.functional import norm
 
 class Dummy_CIDA_Dataset(torch.utils.data.Dataset):
-    def __init__(self, x_shape, domains:list, num_classes:int, num_unique_examples_per_class:int, normalize_domain:int=False) -> None:
+    def __init__(self, x_shape, domains:list, num_classes:int, num_unique_examples_per_class:int, normalize_domain:bool=False) -> None:
         """
         args:
             domain_configs: {
@@ -21,13 +22,15 @@ class Dummy_CIDA_Dataset(torch.utils.data.Dataset):
 
         x_source = np.ones(x_shape, dtype=np.float)
 
-        if normalize_domain != False:
-            domains = map(lambda k: k/normalize_domain, domains)
+        if normalize_domain:
+            min_domain = min(domains)
+            max_domain_after_min = max(domains) - min_domain
+            domains = list(map(lambda x: (x-min_domain)/max_domain_after_min, domains))
 
         for u in domains:
             for y in range(num_classes):
                 for i in range(num_unique_examples_per_class):
-                    x = np.array(x_source * y * u, dtype=np.single)
+                    x = np.array(x_source * u + (y+1), dtype=np.single)
                     u_array = np.array([u], dtype=np.single)
 
                     examples.append((x,y,u_array))
@@ -48,8 +51,13 @@ if __name__ == "__main__":
         x_shape=[2,128],
         domains=[1,2,3,4,5,6],
         num_classes=10,
-        num_unique_examples_per_class=5000
+        num_unique_examples_per_class=5000,
+        normalize_domain=True
     )
 
+    s = set()
+
     for d in ds:
-        print(d)
+        s.add(float(d[2]))
+
+    print(s)
