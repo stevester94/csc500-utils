81497ed51fd477291b10f4d56923658b9964facf
Steven Mackey <stevester94@gmail.com>
2021-10-19 20:14:57 -0700

    Decouple normalization from oshea ds and extensively test

--- steves_utils/oshea_RML2016_ds.py
+++ steves_utils/oshea_RML2016_ds.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
 import pickle
+from unittest.case import SkipTest
 import numpy as np
 import torch
+from torch._C import dtype
 
 
 class OShea_RML2016_DS(torch.utils.data.Dataset):
@@ -24,8 +26,7 @@ class OShea_RML2016_DS(torch.utils.data.Dataset):
     }
     """
     def __init__(self, path:str="/mnt/wd500GB/CSC500/csc500-super-repo/datasets/RML2016.10a_dict.pkl", 
-        snrs_to_get:list=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2],
-        normalize_snr:tuple=None) -> None:
+        snrs_to_get:list=[-20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18]) -> None:
         """
         args:
             domain_configs: {
@@ -53,33 +54,19 @@ class OShea_RML2016_DS(torch.utils.data.Dataset):
 
         Xd = pickle.load(open(path,'rb'), encoding="latin1")
         self.Xd = Xd
-        snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])
-
-        if normalize_snr is not None:
-            assert(normalize_snr[0] < normalize_snr[1])
-            assert(normalize_snr[0] <= min(snrs_to_get))
-            assert(normalize_snr[1] >= max(snrs_to_get))
-
-            min_snr = normalize_snr[0]
-            max_snr_after_min = normalize_snr[1] - min_snr
-            normalizer_func = lambda snr: (snr-min_snr)/max_snr_after_min
-        
+        snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])       
 
         data = []  
         lbl = []
         for mod in mods:
             for snr in snrs:
-
                 if snr in snrs_to_get:
                     for x in Xd[(mod,snr)]:
-                        if normalize_snr:
-                            snr = normalizer_func(snr)
-
                         data.append(
                             (
                                 x.astype(np.single),
                                 self.modulation_to_int(mod),
-                                np.array([snr], dtype=np.single)
+                                np.array([snr], dtype=np.int32)
                             )
                         )
 
@@ -97,7 +84,7 @@ class OShea_RML2016_DS(torch.utils.data.Dataset):
     # 
     @classmethod
     def get_snrs(cls):
-        return [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2]
+        return [-20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
         
 
 
@@ -112,33 +99,120 @@ if __name__ == "__main__":
     class test_OShea_RML2016_DS(unittest.TestCase):
         @classmethod
         def setUpClass(self) -> None:
-            self.ds = OShea_RML2016_DS(normalize_snr=(-20,18))
+            # self.ds = OShea_RML2016_DS()
+            pass
 
-        def test_normalization(self):
-            source_snrs = [-18, -12, -6, 0, 6, 12, 18]
-            target_snrs = [2, 4, 8, 10, -20, 14, 16, -16, -14, -10, -8, -4, -2]
+        @unittest.SkipTest
+        def test_snrs(self):
+            snrs_to_test = [
+                [-18, -12, -6, 0, 6, 12, 18],
+                [2, 4, 8, 10, -20, 14, 16, -16, -14, -10, -8, -4, -2],
+                [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2]
+            ]
+
+            for s in snrs_to_test:
+                s.sort()
+                unique_snrs = set()
+                unique_mods = set()
+                ds = OShea_RML2016_DS(snrs_to_get=s)
+
+                for ex in ds:
+                    unique_snrs.add(ex[2][0])
+                    unique_mods.add(ex[1])
+
+                print(unique_snrs)
+                print(unique_mods)
+
+                unique_snrs = list(unique_snrs)
+                unique_snrs.sort()
 
 
+                self.assertEqual(len(unique_mods), 11)
+                self.assertEqual(s, unique_snrs)
 
-            normalized_source_snrs = list(set([float(x[2]) for x in OShea_RML2016_DS(normalize_snr=(-20, 18), snrs_to_get=source_snrs)]))
-            normalized_source_snrs.sort()
+    class test_normalization(unittest.TestCase):
+        @SkipTest
+        def test_lazy_normalization(self):
+            from steves_utils.utils_v2 import normalize_val, denormalize_val
+            from steves_utils.lazy_map import Lazy_Map
 
-            print(normalized_source_snrs)
+            snrs_to_test = [
+                [-18, -12, -6, 0, 6, 12, 18],
+                [2, 4, 8, 10, -20, 14, 16, -16, -14, -10, -8, -4, -2],
+                [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2]
+            ]
 
-            normalized_target_snrs = list(set([float(x[2]) for x in OShea_RML2016_DS(normalize_snr=(-20, 18), snrs_to_get=target_snrs)]))
-            normalized_target_snrs.sort()
+            for s in snrs_to_test:
+                s.sort()
+                unique_snrs = set()
+                unique_mods = set()
+                ds = OShea_RML2016_DS(snrs_to_get=s)
 
-            print(normalized_target_snrs)
+                mapped_ds = Lazy_Map(ds, 
+                    lambda ex: (
+                        ex[0], ex[1], ex[2], 
+                        np.array([normalize_val(min(s), max(s), ex[2][0])], dtype=np.single)
+                    )
+                )
 
-            # non_normalized_snrs = OShea_RML2016_DS.get_snrs()
-            # normalized_snrs = OShea_RML2016_DS.get_snrs()
-            # normalized_snrs = list(map(lambda snr: (snr - min()) / (max(OShea_RML2016_DS.get_snrs())-min(OShea_RML2016_DS.get_snrs()))))
-            # self.snrs.sort()
+                for ex in mapped_ds:
+                    self.assertAlmostEqual(ex[2][0], denormalize_val(min(s), max(s), ex[3][0]), places=5)
 
-            # for i, snr in enumerate(self.snrs):
-            #     test = 
-            #     self.assertAlmostEqual(s[i], test)
-    
+
+        def test_eyeball_the_snrs(self):
+            from steves_utils.utils_v2 import normalize_val, denormalize_val
+            from steves_utils.lazy_map import Lazy_Map
+
+            MIN = -20
+            MAX = 18
+
+            normalized_original_snrs = list(map(lambda s: normalize_val(MIN, MAX, s), OShea_RML2016_DS.get_snrs()))
+
+            snrs_to_test = [
+                [-20, -18, -16],
+                [ -14, -12, ],
+                [-10, -8, ],
+                [ -6, -4, ],
+                [-2, 0, ],
+                [2, 4, 6, 8, 10, 12, 14, 16, 18],
+            ]
+
+
+            unique_normalized_snrs = set()
+            for s in snrs_to_test:
+                s.sort()
+
+                ds = OShea_RML2016_DS(snrs_to_get=s)
+
+                mapped_ds = Lazy_Map(ds, 
+                    lambda ex: (
+                        ex[0], ex[1], ex[2], 
+                        np.array([normalize_val(MIN, MAX, ex[2][0])], dtype=np.single)
+                    )
+                )
+
+                for ex in mapped_ds:
+                    unique_normalized_snrs.add(ex[3][0])
+            
+            unique_normalized_snrs = list(unique_normalized_snrs)
+            unique_normalized_snrs.sort()
+
+            print(unique_normalized_snrs)
+            print(normalized_original_snrs)
+
+            self.assertEqual(len(unique_normalized_snrs), len(OShea_RML2016_DS.get_snrs()))
+
+            for unique, nrml_original in zip(unique_normalized_snrs, normalized_original_snrs):
+                self.assertAlmostEqual(unique, nrml_original, places=5)
+            
+            for unique, nrml_original in zip(unique_normalized_snrs, OShea_RML2016_DS.get_snrs()):
+                self.assertAlmostEqual(
+                    denormalize_val(MIN, MAX,unique), nrml_original, places=5
+                )
+
+            unique_normalized_snrs
+
+
 
     unittest.main()
     
--- steves_utils/utils_v2.py
+++ steves_utils/utils_v2.py
@@ -2,6 +2,8 @@
 
 import os
 
+from torch._C import dtype
+
 def get_datasets_base_path():
     return os.environ["DATASETS_ROOT_PATH"]
 
@@ -9,3 +11,55 @@ def get_files_with_suffix_in_dir(path, suffix):
     """Returns full path"""
     (_, _, filenames) = next(os.walk(path))
     return [os.path.join(path,f) for f in filenames if f.endswith(suffix)]
+
+def normalize_val(min, max, val):
+    if val < min or val > max:
+        raise Exception("Val is out of range")
+    return float(val - min) / float(max-min)
+
+def denormalize_val(min, max, val):
+    if val > 1.0 or val < 0.0:
+        raise Exception("Val is out of range")
+    return val * (max-min) + min
+
+
+if __name__ == "__main__":
+    import unittest
+    import numpy as np
+    class test_normalization(unittest.TestCase):
+        def test_int_normalization(self):
+            MIN = -1000
+            MAX = 1000
+            COUNT = 1000
+
+            # numbers = np.random.default_rng(1337).choice(list(range(MIN, MAX+1)),COUNT, replace=False)
+            numbers = np.arange(MIN, MAX)
+
+            for n in numbers:
+                nrm = normalize_val(MIN, MAX, n)
+                self.assertTrue(nrm <= 1.0 and nrm >= 0.0)
+
+                dnrm = round(denormalize_val(MIN, MAX, nrm))
+
+                self.assertEqual(n, dnrm)
+
+                print(n, nrm, dnrm)
+
+
+        def test_float_normalization(self):
+            MIN = -1000
+            MAX = 1000
+            COUNT = 10000
+
+            numbers = np.random.default_rng(1337).uniform(MIN, MAX, COUNT)
+
+            for n in numbers:
+                nrm = normalize_val(MIN, MAX, n)
+                self.assertTrue(nrm <= 1.0 and nrm >= 0.0)
+
+                dnrm = denormalize_val(MIN, MAX, nrm)
+
+                self.assertAlmostEqual(n, dnrm)
+
+                print(n, nrm, dnrm)
+    unittest.main()
