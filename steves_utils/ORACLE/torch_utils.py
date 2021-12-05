60353ca9cc91ad2aa29cae6e4af5e3f47ed49452
Steven Mackey <stevester94@gmail.com>
2021-12-05 11:07:44 -0800

    Break out fsl utils, rename oracle.torch (will undoubtedly break other shit)

---
+++ steves_utils/ORACLE/torch_utils.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python3
+
+import math
+import torch
+
+from steves_utils.ORACLE.ORACLE_sequence import ORACLE_Sequence
+from steves_utils.lazy_map import Lazy_Map
+from steves_utils.ORACLE.utils_v2 import serial_number_to_id
+from steves_utils.fsl_utils import split_ds_into_episodes
+
+class ORACLE_Torch_Dataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        desired_serial_numbers,
+        desired_runs,
+        desired_distances,
+        window_length,
+        window_stride,
+        num_examples_per_device,
+        seed,
+        max_cache_size=1e6,
+        transform_func=None,
+        prime_cache=False,
+    ) -> None:
+        super().__init__()
+
+        self.os = ORACLE_Sequence(
+            desired_serial_numbers,
+            desired_runs,
+            desired_distances,
+            window_length,
+            window_stride,
+            num_examples_per_device,
+            seed,
+            max_cache_size,
+            prime_cache=prime_cache
+        )
+
+        self.transform_func = transform_func
+
+    def __len__(self):
+        return len(self.os)
+    
+    def __getitem__(self, idx):
+        if self.transform_func != None:
+            return self.transform_func(self.os[idx])
+        else:
+            return self.os[idx]
+    
+def split_dataset_by_percentage(train:float, val:float, test:float, dataset, seed:int):
+    assert train < 1.0
+    assert val < 1.0
+    assert test < 1.0
+    assert train + val + test <= 1.0
+
+    num_train = math.floor(len(dataset) * train)
+    num_val   = math.floor(len(dataset) * val)
+    num_test  = math.floor(len(dataset) * test)
+
+    return torch.utils.data.random_split(dataset, (num_train, num_val, num_test), generator=torch.Generator().manual_seed(seed))
+
+
+
+def build_ORACLE_episodic_iterable(
+    desired_serial_numbers,
+    desired_distances,
+    desired_runs,
+    window_length,
+    window_stride,
+    num_examples_per_device,
+    seed,
+    max_cache_size,
+    n_shot,
+    n_query,
+    n_train_tasks,
+    n_val_tasks,
+    n_test_tasks,
+):
+
+    ds = ORACLE_Torch_Dataset(
+                    desired_serial_numbers=desired_serial_numbers,
+                    desired_distances=desired_distances,
+                    desired_runs=desired_runs,
+                    window_length=window_length,
+                    window_stride=window_stride,
+                    num_examples_per_device=num_examples_per_device,
+                    seed=seed,  
+                    max_cache_size=max_cache_size,
+                    # transform_func=lambda x: (x["iq"], serial_number_to_id(x["serial_number"]), x["distance_ft"]),
+                    transform_func=lambda x: (torch.from_numpy(x["iq"]), serial_number_to_id(x["serial_number"]), ), # Just (x,y)
+                    prime_cache=False
+    )
+
+    return split_ds_into_episodes(
+        ds=ds,
+        n_way=len(desired_serial_numbers),
+        n_shot=n_shot,
+        n_query=n_query,
+        n_train_tasks=n_train_tasks,
+        n_val_tasks=n_val_tasks,
+        n_test_tasks=n_test_tasks,
+        seed=seed,
+    )
---
+++ steves_utils/fsl_utils.py
@@ -0,0 +1,84 @@
+from math import floor
+from typing import Tuple
+import torch
+from torch import nn
+
+# BEGIN KEEP IN THIS ORDER
+# It's critical that you override this function first before importing the rest of easyfsl
+def my_compute_backbone_output_shape(backbone: nn.Module) -> Tuple[int]:
+    """ 
+    Compute the dimension of the feature space defined by a feature extractor.
+    Args:
+        backbone: feature extractor
+
+    Returns:
+        shape of the feature vector computed by the feature extractor for an instance
+
+    """
+    input_images = torch.ones((4, 2, 128))
+    output = backbone(input_images)
+
+    return tuple(output.shape[1:])
+import easyfsl.utils; easyfsl.utils.compute_backbone_output_shape = my_compute_backbone_output_shape
+from easyfsl.data_tools import EasySet, TaskSampler
+from easyfsl.methods import PrototypicalNetworks, AbstractMetaLearner
+from easyfsl.utils import sliding_average, compute_backbone_output_shape
+# END KEEP IN THIS ORDER
+
+
+def split_ds_into_episodes(
+    ds,
+    n_way,
+    n_shot,
+    n_query,
+    n_train_tasks,
+    n_val_tasks,
+    n_test_tasks,
+    seed,
+):
+    import copy 
+
+    train_len = floor(len(ds)*0.7)
+    val_len   = floor(len(ds)*0.15)
+    test_len  = len(ds) - train_len - val_len
+
+    train_ds, val_ds, test_ds = torch.utils.data.random_split(ds, [train_len, val_len, test_len], generator=torch.Generator().manual_seed(seed))
+
+    train_ds.labels = [ex[1] for ex in train_ds]
+    val_ds.labels   = [ex[1] for ex in val_ds]
+    test_ds.labels  = [ex[1] for ex in test_ds]
+
+    def wrap_in_dataloader(ds,n_tasks):
+        sampler = TaskSampler(
+                ds,
+                n_way=n_way,
+                n_shot=n_shot,
+                n_query=n_query,
+                n_tasks=n_tasks
+            )
+
+        return torch.utils.data.DataLoader(
+            ds,
+            num_workers=6,
+            persistent_workers=True,
+            prefetch_factor=50,
+            # pin_memory=True,
+            batch_sampler=sampler,
+            collate_fn=sampler.episodic_collate_fn
+        )
+
+    # Deep copy necessary because of how tensors are retrieved from workers
+    val_list = []
+    for k in wrap_in_dataloader(val_ds, n_val_tasks):
+        val_list.append(copy.deepcopy(k))
+
+    test_list = []
+    for k in wrap_in_dataloader(test_ds, n_test_tasks):
+        test_list.append(copy.deepcopy(k))
+
+    
+    return (
+        wrap_in_dataloader(train_ds, n_train_tasks),
+        val_list,
+        test_list,
+    )
