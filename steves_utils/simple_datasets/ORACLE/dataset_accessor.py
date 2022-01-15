1af031b0f1cf5534122b78c5bb1d76c8e383fb02
Steven Mackey <stevester94@gmail.com>
2022-01-14 20:34:38 -0800

    Get dataset path properly

--- steves_utils/simple_datasets/ORACLE/dataset_accessor.py
+++ steves_utils/simple_datasets/ORACLE/dataset_accessor.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python3
 import numpy as np
 import pickle
+import os
 from steves_utils.ORACLE.utils_v2 import (
     ALL_DISTANCES_FEET,
     ALL_SERIAL_NUMBERS,
     serial_number_to_id
 )
-
 from steves_utils.simple_datasets.general_dataset import create_datasets_from_stratified_ds
-from steves_utils.utils_v2 import norm
+from steves_utils.utils_v2 import (norm, get_datasets_base_path)
 from steves_utils.simple_datasets.ORACLE.utils import genericize_stratified_dataset
 
 MAX_EXAMPLES = 10000
@@ -18,8 +18,8 @@ def get_dataset(
     distances:list,
     num_examples_per_distance_per_serial:int,
     seed:int,
-    pickle_path:str,
     normalize_type:str=None,
+    pickle_path:str=os.path.join(get_datasets_base_path(), "oracle.stratified_ds.2022A.pkl"),
     train_val_test_percents=(0.7,0.15,0.15)
 )->tuple:
     with open(pickle_path, "rb") as f:
@@ -61,7 +61,6 @@ class Test_Dataset(unittest.TestCase):
             distances=cls.domains,
             num_examples_per_distance_per_serial=cls.n,
             seed=1337,
-            pickle_path="/mnt/wd500GB/CSC500/csc500-super-repo/csc500-dataset-preprocessor/ORACLE/oracle.stratified_ds.2022A.pkl",
             # normalize_type=None,
             # train_val_test_percents=(0.7,0.15,0.15)
         )
