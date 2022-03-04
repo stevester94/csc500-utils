eef048dd54d08ce27ac513cc90e3f0d78c46c402
Steven Mackey <stevester94@gmail.com>
2022-03-04 14:00:00 -0800

    Update oracle for new domains and dataset path

--- steves_utils/stratified_dataset/test/episodic_accessor/test_oracle.py
+++ steves_utils/stratified_dataset/test/episodic_accessor/test_oracle.py
@@ -3,7 +3,7 @@ import unittest
 import os
 
 from steves_utils.ORACLE.utils_v2 import (
-    ALL_DISTANCES_FEET,
+    ALL_DISTANCES_FEET_NARROWED,
     ALL_SERIAL_NUMBERS,
 )
 
@@ -12,7 +12,7 @@ from steves_utils.utils_v2 import get_datasets_base_path
 from steves_utils.stratified_dataset.test.episodic_accessor.common import Test_Episodic_Accessor
 
 
-Test_Episodic_Accessor.domains = ALL_DISTANCES_FEET
+Test_Episodic_Accessor.domains = ALL_DISTANCES_FEET_NARROWED
 Test_Episodic_Accessor.labels  = ALL_SERIAL_NUMBERS
 Test_Episodic_Accessor.num_examples_per_domain_per_label=100
 Test_Episodic_Accessor.dataset_seed=1337
@@ -22,7 +22,7 @@ Test_Episodic_Accessor.n_way=len(ALL_SERIAL_NUMBERS)
 Test_Episodic_Accessor.n_query=2
 Test_Episodic_Accessor.train_val_test_k_factors=(1,2,2)
 Test_Episodic_Accessor.train_val_test_percents=(0.7,0.15,0.15)
-Test_Episodic_Accessor.pickle_path = os.path.join(get_datasets_base_path(), "oracle.stratified_ds.2022A.pkl")
+Test_Episodic_Accessor.pickle_path = os.path.join(get_datasets_base_path(), "/mnt/wd500GB/CSC500/csc500-main/datasets/oracle.Run1_10kExamples_stratified_ds.2022A.pkl")
 
 
 if __name__ == "__main__":
