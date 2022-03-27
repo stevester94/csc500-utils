b2d1c4523f3fcd1c70ba71bc77391c647770a730
Steven Mackey <stevester94@gmail.com>
2022-03-27 13:42:39 -0700

    Better error handling in summary utils

--- steves_utils/summary_utils.py
+++ steves_utils/summary_utils.py
@@ -1,13 +1,22 @@
 import subprocess
-import pandas as pd
 import json
+import os
+from steves_utils.utils_v2 import get_experiments_base_path
+import sys
+
+
 
 
 def parse_results_from_ipynb_dict(d:dict):
     for cell in d["cells"]:
         if "tags" in cell["metadata"] and cell["metadata"]["tags"] == ["experiment_json"]:
-            assert len(cell["outputs"]) == 1
-            assert len(cell["outputs"][0]["data"]["text/plain"]) == 1
+            if len(cell["outputs"]) != 1:
+                print(type(cell["outputs"]))
+                print(cell["outputs"])
+                l  = len(cell["outputs"])
+                raise Exception(f"len of experiment_json unexpected length (got {l})")
+            if len(cell["outputs"][0]["data"]["text/plain"]) != 1:
+                raise Exception("len of cell data unexpected length")
 
             break
 
@@ -25,8 +34,14 @@ def get_experiments_from_path(start_path):
 
     for p in experiment_dot_json_paths:
         with open(p) as f:
-            experiments.append(
-                parse_results_from_ipynb_dict(json.load(f))
-            )
+            try:
+                experiments.append(
+                    parse_results_from_ipynb_dict(json.load(f))
+                )
+            except:
+                print("Error parsing",p, file=sys.stderr)
+                raise
+
     
     return experiments
+
